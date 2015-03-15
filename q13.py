8987b27cc9a1e299ad4ac02346d87c90a1800b69
Matt Byrne <matt.byrne@yahoo.co.uk>
2015-03-15 08:04:47 +0000

    q13 answered

--- q13.py
+++ q13.py
@@ -6,14 +6,23 @@
 import unittest
 
 def max_in_list(l):
+	largest = l[0]
 
-	pass
+	for item in l:
+		if item > largest:
+			largest = item
+
+	return largest
 
 class test_max_in_list(unittest.TestCase):
 
 	def test_mem(self):
 		res_01 = max_in_list([1, 33, 21, 42, 99])
+		res_02 = max_in_list([1001, 33, 21, 42, 99])
+		res_03 = max_in_list([1, 3399, 21, 42, 99])
 		self.assertEqual(res_01, 99)
+		self.assertEqual(res_02, 1001)
+		self.assertEqual(res_03, 3399)
 
 if __name__ == '__main__':
 	unittest.main()
